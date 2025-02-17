8e6fc4acf28a9a5fbc27bc29d0a76c40ba8be025
Ajay Gangwar <ajaygangwar24598@gmail.com>
2025-02-17 13:30:02 +0530

    added week 1 deep thought  assignment

---
+++ week1/deep/deep.py
@@ -0,0 +1,8 @@
+ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+
+if ans == "42" or ans == "forty-two" or ans == "forty two":
+    print("Yes")
+else:
+    print("No")
+
+
